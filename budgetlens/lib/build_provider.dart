029b6e4d849549e3bc934f61d3eef3a5d9a98251
Bhargava K K <bhargavakk13@gmail.com>
2025-11-30 00:49:20 +0530

    Linked provider to main

--- budgetlens/lib/build_provider.dart
+++ budgetlens/lib/build_provider.dart
@@ -10,4 +10,34 @@ class BudgetProvider extends ChangeNotifier{ // ChangeNotifier to allow state ma
     calculator.addExpense(state, amount);
     notifyListeners(); // Notify listeners about state changes
   }
+
+  void addIncome(double amount) {
+    calculator.addIncome(state, amount);
+    notifyListeners();
+  }
+
+  void switchMode() {
+    calculator.switchMode(state);
+    notifyListeners();
+  }
+
+  double get displayAmount {
+    if(state.viewMode){
+      return calculator.getRemainingToday(state);
+    }else{
+      return calculator.getRemainingBudget(state);
+    }
+  }
+
+  int get daysRemaining {
+    return calculator.getDaysRemaining(state);
+  }
+
+  double get dailyAllowance {
+    return calculator.getDailyAllowance(state);
+  }
+
+  double get remainingBudget {
+    return calculator.getRemainingBudget(state);
+  }
 }
