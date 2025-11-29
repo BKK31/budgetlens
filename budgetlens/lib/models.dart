01e11f39fe7f9a853ebcf3c5d502f0a3028155ff
Bhargava K K <bhargavakk13@gmail.com>
2025-11-29 20:04:35 +0530

    Initial logic implemented

---
+++ budgetlens/lib/models.dart
@@ -0,0 +1,9 @@
+class BudgetState{
+  double totalBudget = 0.0;
+  double totalSpent = 0.0;
+  bool viewMode = true;
+  DateTime budgetStartDate = DateTime.now();
+  DateTime budgetEndDate = DateTime.now();
+  double todaysSpend = 0.0;
+}
+
