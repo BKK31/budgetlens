008298878f46b677565b5135e0beba0e796baa94
Bhargava K K <bhargavakk13@gmail.com>
2026-02-08 14:43:40 +0530

    Updated package name and fastlane content

---
+++ budgetlens/android/app/src/main/kotlin/com/budget/lens/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.budget.lens
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
--- budgetlens/android/app/src/main/kotlin/com/example/budgetlens/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.budgetlens
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
